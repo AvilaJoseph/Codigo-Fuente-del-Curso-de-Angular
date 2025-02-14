87b973a0c2358290893de3a908e873b211458370
AvilaJoseph <avilajoseph2021@gmail.com>
2025-02-14 00:19:34 -0500

    Paquetes UUID

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid'
+
+console.log(uuid())
+
 import { character } from '../interfaces/character.interfaces';
 
 @Injectable({
@@ -7,14 +11,17 @@ import { character } from '../interfaces/character.interfaces';
 export class DbzService {
 
   public characters: character[] = [{
+      id: uuid(),
       name:'Krillin',
       power:1000
   },
   {
+      id: uuid(),
       name:'Goku',
       power: 9500
   },
   {
+      id: uuid(),
       name:'Vegeta',
       power: 9200
   }
